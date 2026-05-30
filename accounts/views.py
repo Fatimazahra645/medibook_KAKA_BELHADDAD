5b4b648d99100720cbbb2a6e4938fe71a483dfae
Fatimazahra645 <fz0197102@gmail.com>
2026-05-30 18:02:58 +0100

    119

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+        user = authenticate(
+            username=request.POST['username'],
+            password=request.POST['password']
+        )
+        if user:
+            login(request, user)
+            return redirect('home')
+
+    return render(request, "accounts/login.html")
