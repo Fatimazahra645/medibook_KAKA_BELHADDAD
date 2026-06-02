1fedf232059cb9f54e7360545b7cf1f163e87d19
Oumaima-bel <oumaimabalhaddad204@gmail.com>
2026-06-02 20:50:39 +0100

    cleaning templates and fixing admin dashboard issus

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,6 +1,9 @@
 def get_dashboard_url(user):
-    if user.is_superuser or getattr(user,'role','') == "ADMIN":
+
+    if user.is_superuser:
         return "/dashboard/admin/"
-    if getattr(user,'role','') == "DOCTOR":
+    if getattr(user, 'role', '') == "ADMIN":
+        return "/dashboard/admin/"
+    if getattr(user, 'role', '') == "DOCTOR":
         return "/dashboard/doctor/"
     return "/dashboard/patient/"
