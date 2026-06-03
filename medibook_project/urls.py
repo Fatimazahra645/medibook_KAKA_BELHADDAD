435af86317f4d9d300b3b9f38043a0813ff1cf04
Oumaima-bel <oumaimabalhaddad204@gmail.com>
2026-06-03 12:56:17 +0100

    solved error

--- medibook_project/urls.py
+++ medibook_project/urls.py
@@ -4,8 +4,6 @@ from accounts import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
-u
 from django.views.static import serve
 from django.urls import re_path
 
